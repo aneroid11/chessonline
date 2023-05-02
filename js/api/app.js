786f05dd8a36895db530281fa564fa0309d2ea57
aneroid11 <a-e-v2003@yandex.by>
2023-05-02 23:28:27 +0300

    js works

---
+++ js/api/app.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDCUea-F9S2qmzHY3ib0Paav9dBYq2rXYI",
+    authDomain: "aneroid11-chess.firebaseapp.com",
+    projectId: "aneroid11-chess",
+    storageBucket: "aneroid11-chess.appspot.com",
+    messagingSenderId: "506748179483",
+    appId: "1:506748179483:web:f7f5f69c0e6c1a812d94be"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+let headerSiteName = document.getElementById("site-name")
+headerSiteName.textContent = "jojo"
+
+console.log('hello world!')
