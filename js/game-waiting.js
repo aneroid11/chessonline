b5761a367a3b38da5f5c8808cffa9dd911c6924a
aneroid11 <a-e-v2003@yandex.by>
2023-05-03 11:47:03 +0300

    game-waiting.js

---
+++ js/game-waiting.js
@@ -0,0 +1,21 @@
+console.log("create a game!")
+
+const queryString = window.location.search
+const urlParams = new URLSearchParams(queryString)
+
+if (urlParams.has("time-limit")) {
+    // this is a game creation
+    let timeLimit = urlParams.get("time-limit")
+    let color = urlParams.get("color")
+    console.log("create game " + timeLimit + " " + color)
+}
+else if (urlParams.has("game-id")) {
+    // this is a connection to an existing game
+    let gameId = urlParams.get("game-id")
+    console.log("connect to game " + gameId)
+}
+else {
+    console.log("wtf?")
+}
+
+// console.log(urlParams.get("time-limit"))
