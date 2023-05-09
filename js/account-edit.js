6b385cc90ca993edd5e6ffc5bdc78f903c2a753b
aneroid11 <a-e-v2003@yandex.by>
2023-05-09 13:25:46 +0300

    get user name into account-edit.html

--- js/account-edit.js
+++ js/account-edit.js
@@ -1,5 +1,9 @@
 import { userIsAuthenticated } from "./app.js";
+import { getUserProfileInfo } from "./api/users.js";
 
 if (!userIsAuthenticated()) {
     window.location.href = "login.html"
 }
+
+const profileInfo = await getUserProfileInfo()
+document.getElementById("acc-edit-name").value = profileInfo.name
