4796866d42cc3d2aae931e997e8a9e8849ef2dce
aneroid11 <a-e-v2003@yandex.by>
2023-05-09 17:42:39 +0300

    add real chessboard

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 import {userIsAuthenticated} from "./app.js";
+import {Chessboard} from "https://cdn.jsdelivr.net/npm/cm-chessboard@7/src/cm-chessboard/Chessboard.js";
 
 if (userIsAuthenticated()) {
-    window.location.replace("index-logged.html")
+    window.location.replace("index-logged.html");
 }
+
+const board = new Chessboard(document.getElementById("chess-board"));
