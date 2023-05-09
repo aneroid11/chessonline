d7934f59d78084fbc7a5237e2e6404c6f2852af2
aneroid11 <a-e-v2003@yandex.by>
2023-05-09 13:54:36 +0300

    account edit form input validation

--- js/account-edit.js
+++ js/account-edit.js
@@ -2,8 +2,39 @@ import { userIsAuthenticated } from "./app.js";
 import { getUserProfileInfo } from "./api/users.js";
 
 if (!userIsAuthenticated()) {
-    window.location.href = "login.html"
+    window.location.replace("login.html")
 }
 
+const saveChangesButton = document.getElementById("acc-edit-save-changes")
+const passwordInput = document.getElementById("acc-edit-password")
+const nameInput = document.getElementById("acc-edit-name")
+const newPasswordInput = document.getElementById("acc-edit-new-password")
+const repeatPasswordInput = document.getElementById("acc-edit-repeat-password")
+
+function saveChanges(event) {
+    event.preventDefault()
+
+    alert("save changes")
+    window.location.replace("account.html")
+}
+
+function checkPasswordLength(event) {
+    event.preventDefault()
+
+    saveChangesButton.disabled = passwordInput.value.length < 6
+}
+
+function compareNewPasswordAndRepeatPassword(event) {
+    event.preventDefault()
+
+    saveChangesButton.disabled = newPasswordInput.value !== repeatPasswordInput.value
+}
+
+saveChangesButton.disabled = true
+passwordInput.addEventListener("input", checkPasswordLength)
+document.getElementById("acc-edit-form").addEventListener("submit", saveChanges)
+newPasswordInput.addEventListener("input", compareNewPasswordAndRepeatPassword)
+repeatPasswordInput.addEventListener("input", compareNewPasswordAndRepeatPassword)
+
 const profileInfo = await getUserProfileInfo()
-document.getElementById("acc-edit-name").value = profileInfo.name
+nameInput.value = profileInfo.name
