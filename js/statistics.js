ab299da32d241076ccb0b91e004b3522302f2934
aneroid11 <a-e-v2003@yandex.by>
2023-05-11 14:49:04 +0300

    small changes

--- js/statistics.js
+++ js/statistics.js
@@ -9,11 +9,10 @@ async function main() {
     // достать все комнаты, в которых мы есть и в которых есть result.
     // достать все комнаты.
     const allRooms = await getAllRooms();
+    const myUid = document.cookie.split(";")[0].split("=")[1];
 
     for (const [key, value] of Object.entries(allRooms)) {
-        // alert(key value);
-        // alert(key);
-        if (value["statistics-string"] !== undefined) {
+        if (value["statistics-string"] !== undefined && (myUid === value["white"] || myUid === value["black"])) {
             // alert(value["statistics-string"]);
             const newArticle = document.createElement("article");
             let htmlStr = "";
@@ -26,7 +25,13 @@ async function main() {
                 document.getElementById("statistics-draws-list").appendChild(newArticle);
             }
             else {
-                document.getElementById("statistics-wins-list").appendChild(newArticle);
+                if (value["result"] === "white" && myUid === value["white"] ||
+                    value["result"] === "black" && myUid === value["black"]) {
+                    document.getElementById("statistics-wins-list").appendChild(newArticle);
+                }
+                else {
+                    document.getElementById("statistics-loses-list").appendChild(newArticle);
+                }
             }
         }
     }
